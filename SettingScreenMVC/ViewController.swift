22617a58c9137426da3d1126412b33b58b283e49
Nikita Shilyaev <shilyaef@iCloud.com>
2023-10-24 13:35:19 +0800

    Initial Commit

---
+++ SettingScreenMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingScreenMVC
+//
+//  Created by Никита Шиляев on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
